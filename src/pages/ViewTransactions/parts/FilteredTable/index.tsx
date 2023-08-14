200a3663520db6bfd0994737762bd86a5cf93283
Beatriz Cavallieri <bfcavallieri@gmail.com>
2023-08-14 08:24:55 -0300

    refac: moves Table because it is not a component

--- src/pages/ViewTransactions/parts/FilteredTable/index.tsx
+++ src/pages/ViewTransactions/parts/FilteredTable/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Table from '../../../../components/Table'
+import Table from '../Table'
 import { useFilterContext } from '../context'
 
 const FilteredTable = (props) => {
--- src/pages/ViewTransactions/parts/Table/index.jsx
+++ src/pages/ViewTransactions/parts/Table/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles.css'
-import { formatCurrency } from '../../functions/formatCurrency'
+import { formatCurrency } from '../../../../functions/formatCurrency'
 
 const Table = (props) => {
     const summary = props.transactions.reduce((total, transaction) => {
