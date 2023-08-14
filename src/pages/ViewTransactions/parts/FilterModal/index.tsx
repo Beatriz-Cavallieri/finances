199f27d1837246f313f4a90fa9af8a317ce170c2
Beatriz Cavallieri <bfcavallieri@gmail.com>
2023-08-13 21:30:04 -0300

    feat: adds clear button to transactions filtering modal

--- src/pages/ViewTransactions/parts/FilterModal/index.tsx
+++ src/pages/ViewTransactions/parts/FilterModal/index.tsx
@@ -3,6 +3,7 @@ import RadioGroup from '../../../../components/RadioGroup'
 import Input from '../../../../components/Input'
 import Modal from '../../../../components/Modal'
 import { useFilterContext } from '../context'
+import './styles.css'
 
 const FilterModal = (props) => {
     const { transactionType, setTransactionType, category, setCategory } = useFilterContext()
@@ -29,21 +30,31 @@ const FilterModal = (props) => {
         props.onClose()
     }
 
+    const clearFilters = () => {
+        setFormData({ category: '', transactionType: 'all' })
+    }
+
     return (
         <Modal isOpen={props.isOpen} onClose={props.onClose}>
-            <RadioGroup
-                name="transaction-type"
-                label="Tipo de transação:"
-                inputs={[
-                    { value: "income", label: "Entrada" },
-                    { value: "expense", label: "Saída" },
-                    { value: "all", label: "Todas" }
-                ]}
-                value={formData.transactionType}
-                onChange={handleTransactionTypeChange} />
-            <Input type="text" id="category" label="Categoria:"
-                onChange={handleCategoryChange} value={formData.category} />
-            <button type='button' onClick={submitFilters}>Filtrar</button>
+            <div className='filterModalContent'>
+                <h2>Filtrar</h2>
+                <RadioGroup
+                    name="transaction-type"
+                    label="Tipo de transação:"
+                    inputs={[
+                        { value: "income", label: "Entrada" },
+                        { value: "expense", label: "Saída" },
+                        { value: "all", label: "Todas" }
+                    ]}
+                    value={formData.transactionType}
+                    onChange={handleTransactionTypeChange} />
+                <Input type="text" id="category" label="Categoria:"
+                    onChange={handleCategoryChange} value={formData.category} />
+            </div>
+            <div className='filterModalFooter'>
+                <button type='button' onClick={submitFilters}>Filtrar</button>
+                <button type='button' onClick={clearFilters}>Limpar filtros</button>
+            </div>
         </Modal>
     )
 }
