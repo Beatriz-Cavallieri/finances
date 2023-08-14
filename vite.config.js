8e9e1d1059865d2cd5baa5510c73b45c2464810d
Beatriz Cavallieri <bfcavallieri@gmail.com>
2023-08-13 23:00:49 -0300

    adds server configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Pasta de saída desejada
+  },
+  server: {
+    proxy: {
+      '/': 'https://beatriz-cavallieri-finances.netlify.app',
+    },
+  },
 })
+
